1f433ef76df21b9942e8ecbdd0932332ffddfa86
Shinya Yaginuma <yaginuuun@gmail.com>
2023-12-20 23:18:52 +0900

    numbering

---
+++ tutorials/01-basics/01-pytorch_basics/01-example1.py
@@ -0,0 +1,29 @@
+import torch
+
+# Create tensors.
+x = torch.tensor(1., requires_grad=True)
+w = torch.tensor(2., requires_grad=True)
+b = torch.tensor(3., requires_grad=True)
+
+# Build a computational graph.
+y = w * x + b    # y = 2 * x + 3
+
+# Compute gradients.
+y.backward()
+
+# Print out the gradients.
+print(x.grad)    # x.grad = 2
+print(w.grad)    # w.grad = 1
+print(b.grad)    # b.grad = 1
+
+# ---------------------------
+# Excercises
+# ---------------------------
+
+# define tensors
+
+# define computation
+
+# compute gradients
+
+# check gradients
---
+++ tutorials/01-basics/01-pytorch_basics/README.md
@@ -0,0 +1,9 @@
+# 1-1. PyTorch Basics
+
+1. Basic autograd example 1
+2. Basic autograd example 2
+3. Loading data from numpy
+4. Input pipline
+5. Input pipline for custom dataset
+6. Pretrained model
+7. Save and load model
