31e08519379a5f218f9b45fe833cbac1f489d327
Shinya Yaginuma <yaginuuun@gmail.com>
2023-12-23 16:17:24 +0900

    split main.py

---
+++ tutorials/01-basics/01-pytorch_basics/07-save-load-model.py
@@ -0,0 +1,15 @@
+# Save and load the model
+
+import torch
+import torchvision
+
+# Load pretrained model
+resnet = torchvision.models.resnet18(pretrained=True)
+
+# Save and load the entire model.
+torch.save(resnet, "model.ckpt")
+model = torch.load("model.ckpt")
+
+# Save and load only the model parameters (recommended).
+torch.save(resnet.state_dict(), "params.ckpt")
+resnet.load_state_dict(torch.load("params.ckpt"))
